3c9dddf227ce9d38f60d22d18bdb53ba52929bb8
nimmehra <nmehra2@sapient.com>
2020-06-16 17:30:49 +0530

    change the component name AUX to Auxiliary

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import  Aux from '../../hoc/Aux' ;
+import  Aux from '../../hoc/Auxiliary' ;
 import './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Modal.css';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
 
